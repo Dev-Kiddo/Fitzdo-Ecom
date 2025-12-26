9b0cc459a9347d1a052b2ddb6805017962312b31
Dev-Kiddo <buzzwithprasanth@gmail.com>
2025-12-26 10:05:41 +0530

    fix: cors url updated

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173", "https://fitzdo-ecom.vercel.app/"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://fitzdo-ecom.vercel.app"], credentials: true }));
 
 // Routes
 app.use("/api/v1", productRouter);
